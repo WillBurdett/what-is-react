8d974164f09c8347f6267a9643086226f0b6d2a8
WillBurdett <williamburdett1995@gmail.com>
2024-03-03 11:30:56 +0000

    using dynamically rendering components meaningfully

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { CORE_CONCEPTS } from "./data";
 import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
+import { EXAMPLES } from './data.js'
 
 // this is a hook or hook function
 // must be inside component functions or other (custom) hooks
@@ -18,7 +19,7 @@ function App() {
   // this is where 'state' comes in
   let tabContent = "Please click a button";
 
-  const [selectedTopic, setSelectedTopic] = useState("Please click button");
+  const [selectedTopic, setSelectedTopic] = useState("components");
   function handleSelect(selectedButton) {
     // this schedules the updated value
     // it will not be available till the component updates
@@ -56,7 +57,15 @@ function App() {
             <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
             <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
-          {selectedTopic}
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>
+              {EXAMPLES[selectedTopic].code}
+              </code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
